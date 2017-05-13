c60c009a81ea5920fa0e7b7cf665d4463ead369b
Brandon <brandonbsuen@gmail.com>
2017-05-13 10:42:56 -0700

    Home page almost finished

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-	$('.container-fluid').fullpage();
+	$(".responsive").each(function() {
+		if ($(this).width() > 602) {
+			$(this).children("img").addClass("logo");
+		} else {
+			$(this).children("img").css("width", "80%");
+		}
+	});
+	var arr;
+	var max;
+	$(".sameheight").each(function() {
+		arr = [];
+		$(this).find(".card").each(function() {
+			arr.push($(this).height());
+		});
+		max = Math.max.apply(null, arr);
+		$(this).find(".card").css("height", max + "px");
+	});
 });
