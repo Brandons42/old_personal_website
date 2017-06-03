73d9cc112ec33046aa7520cb39be8c3da65d7c7f
Brandon <brandonbsuen@gmail.com>
2017-06-02 17:37:51 -0700

    Gulp, Sass, Autoprefixer, Jasmine, Phantom implemented

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var autoprefixer = require("gulp-autoprefixer");
+var jasmine = require("gulp-jasmine-phantom");
+gulp.task("serve", ["sass"], function() {
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch("sass/**/*.scss", ["sass"]);
+    gulp.watch("index.html").on("change", browserSync.reload);
+});
+gulp.task("sass", function() {
+    return gulp.src("sass/**/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer({
+          browsers: ["last 2 versions"]
+        }))
+        .pipe(gulp.dest("./css"))
+        .pipe(browserSync.stream());
+});
+gulp.task("tests", function() {
+  gulp.src("spec/spec.js")
+    .pipe(jasmine({
+      integration: true,
+      vendor: "js/**/*.js"
+    }));
+});
+gulp.task('default', ['serve']);
